bd44c4d662c4034e603770c27d4f778c1ef57b81
hanekawaaircraft <sts126@126.com>
2020-03-01 15:20:25 +0800

    add login.vue & mydb.sql

---
+++ Login.vue
@@ -0,0 +1,274 @@
+<template>
+  <div class="login_container">
+    <div id="bgd">
+      <canvas
+        id='myCanvas'
+        :width='width'
+        :height='height'
+      >
+      </canvas>
+    </div>
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/login.png" alt="">
+      </div>
+      <Form ref="loginFormRef" :model="loginForm" :rules="loginRules" class="login_form">
+        <FormItem prop="user">
+          <Input type="text" v-model="loginForm.username" placeholder="用户名">
+            <Icon type="ios-person-outline" slot="prepend"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem prop="password">
+          <Input v-model="loginForm.password" type="password" placeholder="密码">
+            <Icon type="ios-lock-outline" slot="prepend"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem class="btns"> 
+          <!-- <Checkbox v-model="checked">记住密码</Checkbox> -->
+          <Button type="primary" @click="login">登录</Button>
+        </FormItem>
+      </Form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      canvas: null,
+      context: null,
+      stars: [], //星星数组
+      shadowColorList: [
+        "#39f",
+        "#ec5707",
+        "#b031d4",
+        "#22e6c7",
+        "#92d819",
+        "#14d7f1",
+        "#e23c66"
+        ], //阴影颜色列表
+        directionList: ["leftTop", "leftBottom", "rightTop", "rightBottom"], //星星运行方向
+        speed: 50, //星星运行速度
+        last_star_created_time: new Date(), //上次重绘星星时间
+        Ball: class Ball {
+        constructor(radius) {
+          this.x = 0;
+          this.y = 0;
+          this.radius = radius;
+          this.color = "";
+          this.shadowColor = "";
+          this.direction = "";
+        }
+        draw(context) {
+          context.save();
+          context.translate(this.x, this.y);
+          context.lineWidth = this.lineWidth;
+          var my_gradient = context.createLinearGradient(0, 0, 0, 8);
+          my_gradient.addColorStop(0, this.color);
+          my_gradient.addColorStop(1, this.shadowColor);
+          context.fillStyle = my_gradient;
+          context.beginPath();
+          context.arc(0, 0, this.radius, 0, Math.PI * 2, true);
+          context.closePath();
+          context.shadowColor = this.shadowColor;
+          context.shadowOffsetX = 0;
+          context.shadowOffsetY = 0;
+          context.shadowBlur = 10;
+          context.fill();
+          context.restore();
+        }
+      }, //工厂模式定义Ball类
+      width: window.innerWidth,
+      height: window.innerHeight,
+      //记住密码默认状态
+      // checked: false,
+      // 这是登录表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 这是表单的验证规则对象
+      loginRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+   mounted() {
+    this.canvas = document.getElementById("myCanvas");
+    this.context = this.canvas.getContext("2d");
+    this.createStar(true);
+    this.drawFrame();
+  },
+  methods: {
+    //重复动画
+    drawFrame() {
+      let animation = requestAnimationFrame(this.drawFrame);
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.createStar(false);
+      this.stars.forEach(this.moveStar);
+    },
+    //展示所有的星星
+     createStar(params) {
+       let now = new Date();
+       if (params) {
+         //初始化星星
+         for (var i = 0; i < 50; i++) {
+           const radius = Math.random() * 3 + 2;
+           let star = new this.Ball(radius);
+           star.x = Math.random() * this.canvas.width + 1;
+           star.y = Math.random() * this.canvas.height + 1;
+           star.color = "#ffffff";
+           star.shadowColor = this.shadowColorList[
+             Math.floor(Math.random() * this.shadowColorList.length)
+           ];
+           star.direction = this.directionList[
+             Math.floor(Math.random() * this.directionList.length)
+           ];
+           this.stars.push(star);
+         }
+       } else if (!params && now - this.last_star_created_time > 3000) {
+         //每隔3秒重绘修改颜色其中30个球阴影颜色
+         for (var i = 0; i < 30; i++) {
+           this.stars[i].shadowColor = this.shadowColorList[
+             Math.floor(Math.random() * this.shadowColorList.length)
+           ];
+         }
+         this.last_star_created_time = now;
+       }
+     },
+     //移动
+     moveStar(star, index) {
+       if (star.y - this.canvas.height > 0) {
+         //触底
+         if (Math.floor(Math.random() * 2) === 1) {
+           star.direction = "leftTop";
+         } else {
+           star.direction = "rightTop";
+         }
+       } else if (star.y < 2) {
+         //触顶
+         if (Math.floor(Math.random() * 2) === 1) {
+           star.direction = "rightBottom";
+         } else {
+           star.direction = "leftBottom";
+         }
+       } else if (star.x < 2) {
+         //左边
+         if (Math.floor(Math.random() * 2) === 1) {
+           star.direction = "rightTop";
+         } else {
+           star.direction = "rightBottom";
+         }
+       } else if (star.x - this.canvas.width > 0) {
+         //右边
+         if (Math.floor(Math.random() * 2) === 1) {
+           star.direction = "leftBottom";
+         } else {
+           star.direction = "leftTop";
+         }
+       }
+       if (star.direction === "leftTop") {
+         star.y -= star.radius / this.speed;
+         star.x -= star.radius / this.speed;
+       } else if (star.direction === "rightBottom") {
+         star.y += star.radius / this.speed;
+         star.x += star.radius / this.speed;
+       } else if (star.direction === "leftBottom") {
+         star.y += star.radius / this.speed;
+         star.x -= star.radius / this.speed;
+       } else if (star.direction === "rightTop") {
+         star.y -= star.radius / this.speed;
+         star.x += star.radius / this.speed;
+       }
+       star.draw(this.context);
+     },
+    //登录验证
+    login() {
+      this.$Loading.start();
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        // { data : res } 从axios封装好的包中返回的对象中解构出data 并且重命名为res
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) {
+          return this.$message.error('登录失败！')
+          this.$Loading.error();
+        }
+        else{
+          this.$message.success('登录成功')
+          this.$Loading.finish();
+        }
+        // 1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
+        //   1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
+        //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+        let user=res.data.username
+        // console.log(user)
+        window.sessionStorage.setItem('token', res.data.token)
+        window.sessionStorage.setItem('user',res.data.username)
+        this.$router.push('/home')
+      })
+    },
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-image: -webkit-linear-gradient(left , #4c0785, #9b1976);
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 250px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 120px;
+    width: 120px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    bottom: 6%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px 0 70px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
